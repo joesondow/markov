f921b7d644f6851668bcde68fd09775bfffef603
Joe Sondow <joesondow@users.noreply.github.com>
2017-02-17 13:19:06 -0800

    Initial commit. Basic stuff that goes in a Lambda-invocable Twitter bot function.

---
+++ src/main/java/sondow/twitter/LambdaRequestHandler.java
@@ -0,0 +1,58 @@
+package sondow.twitter;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
+
+/**
+ * The function that AWS Lambda will invoke.
+ *
+ * @author @JoeSondow
+ */
+public class LambdaRequestHandler implements RequestHandler<Object, Object> {
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see com.amazonaws.services.lambda.runtime.RequestHandler#handleRequest(java. lang.Object,
+     * com.amazonaws.services.lambda.runtime.Context)
+     */
+    public Object handleRequest(Object input, Context context) {
+        Configuration config = configure();
+        Tweeter tweeter = new Tweeter(config);
+        String message = "";
+        return tweeter.tweet(message);
+    }
+
+    /**
+     * AWS Lambda only allows underscores in environment variables, not dots, so the default ways twitter4j finds
+     * keys aren't possible. Instead, write your own code that gets the configuration either from Lambda-friendly
+     * environment variables or from a default twitter4j.properties file at the project root, or on the
+     * classpath, or in WEB-INF.
+     *
+     * @return configuration containing Twitter authentication strings
+     */
+    private Configuration configure() {
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        String consumerKeyEnvVar = System.getenv("twitter4j_oauth_consumerKey");
+        String consumerSecretEnvVar = System.getenv("twitter4j_oauth_consumerSecret");
+        String accessTokenEnvVar = System.getenv("twitter4j_oauth_accessToken");
+        String accessTokenSecretEnvVar = System.getenv("twitter4j_oauth_accessTokenSecret");
+        if (consumerKeyEnvVar != null) {
+            cb.setOAuthConsumerKey(consumerKeyEnvVar);
+        }
+        if (consumerSecretEnvVar != null) {
+            cb.setOAuthConsumerSecret(consumerSecretEnvVar);
+        }
+        if (accessTokenEnvVar != null) {
+            cb.setOAuthAccessToken(accessTokenEnvVar);
+        }
+        if (accessTokenSecretEnvVar != null) {
+            cb.setOAuthAccessTokenSecret(accessTokenSecretEnvVar);
+        }
+        Configuration config = cb.build();
+        return config;
+    }
+}
---
+++ src/main/java/sondow/twitter/Tweeter.java
@@ -0,0 +1,52 @@
+package sondow.twitter;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
+
+/**
+ * Interacts with the twitter API to post tweets or do other twitter actions for a given Twitter account.
+ *
+ * @author @JoeSondow
+ */
+public class Tweeter {
+
+    private Configuration config;
+
+    /**
+     * Creates an instance to interact with the Twitter accounts the is set up as the default for this project.
+     */
+    public Tweeter() {
+        this(new ConfigurationBuilder().build());
+    }
+
+    /**
+     * Creates an instance to interact with the Twitter account specified by the supplied configuration.
+     *
+     * @param config the Twitter configuration to use to authenticate
+     */
+    public Tweeter(Configuration config) {
+        this.config = config;
+    }
+
+    /**
+     * Posts a tweet.
+     *
+     * @param message the content to tweet
+     * @return the full tweet object that was created
+     */
+    public Status tweet(String message) {
+        Twitter twitter = new TwitterFactory(config).getInstance();
+        try {
+            Status status = twitter.updateStatus(message);
+            System.out.println("Successfully tweeted message: " + message + " with status " + status);
+            return status;
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
