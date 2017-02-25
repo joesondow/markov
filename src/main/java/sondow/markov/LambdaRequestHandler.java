a01b7a6dd722f0de47b33abab3089e4db1576e57
Joe Sondow <joesondow@users.noreply.github.com>
2017-02-25 13:28:41 -0800

    Renamed base project to markov

--- src/main/java/sondow/markov/LambdaRequestHandler.java
+++ src/main/java/sondow/markov/LambdaRequestHandler.java
@@ -1,4 +1,4 @@
-package sondow.twitter;
+package sondow.markov;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
--- src/main/java/sondow/markov/Tweeter.java
+++ src/main/java/sondow/markov/Tweeter.java
@@ -1,4 +1,4 @@
-package sondow.twitter;
+package sondow.markov;
 
 import twitter4j.Status;
 import twitter4j.Twitter;
